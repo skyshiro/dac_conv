690b64fd42e416aff0b7c96d0d9306bf68cf7d89
Woof <hodge.tannis@gmail.com>
2015-03-04 23:13:00 -0800

    Converted registers
    Code compiles, need to do a hardware test

---
+++ Debug/main.pp
@@ -0,0 +1,17 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: D:/ti4/ccsv5/ccs_base/msp430/include/msp430.h
+main.obj: D:/ti4/ccsv5/ccs_base/msp430/include/msp430f5529.h
+main.obj: D:/ti4/ccsv5/ccs_base/msp430/include/in430.h
+main.obj: D:/ti4/ccsv5/tools/compiler/msp430_4.2.1/include/intrinsics.h
+main.obj: D:/ti4/ccsv5/tools/compiler/msp430_4.2.1/include/stdlib.h
+main.obj: D:/ti4/ccsv5/tools/compiler/msp430_4.2.1/include/linkage.h
+
+../main.c: 
+D:/ti4/ccsv5/ccs_base/msp430/include/msp430.h: 
+D:/ti4/ccsv5/ccs_base/msp430/include/msp430f5529.h: 
+D:/ti4/ccsv5/ccs_base/msp430/include/in430.h: 
+D:/ti4/ccsv5/tools/compiler/msp430_4.2.1/include/intrinsics.h: 
+D:/ti4/ccsv5/tools/compiler/msp430_4.2.1/include/stdlib.h: 
+D:/ti4/ccsv5/tools/compiler/msp430_4.2.1/include/linkage.h: 
